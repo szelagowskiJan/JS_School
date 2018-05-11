71e50d5a9a586b5ea0bf8b58d1b08cd53061ef17
szelagowskiJan <szelagowski.jan1@gmail.com>
2018-05-11 22:37:05 +0200

    table

--- MyProject/src/main/java/pl/coderslab/security/SecurityConfig.java
+++ MyProject/src/main/java/pl/coderslab/security/SecurityConfig.java
@@ -1,39 +1,68 @@
 package pl.coderslab.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/").permitAll()
-//        .antMatchers("/**").permitAll()
+    private static String REALM = "MY_TEST_REALM";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+        //.antMatchers("/**").permitAll()
 //        .antMatchers("/**/js/**").permitAll()
-        .antMatchers("/**/w3images/**").permitAll()
-        .antMatchers("/**/secured/**").access("hasRole('USER')")
-        .antMatchers("/**/admin/**").access("hasRole('ADMIN')")
-        .and()
-        .formLogin()
-        .loginPage("/login")
-        .failureUrl("/login-error");
+                .antMatchers("/**/w3images/**").permitAll()
+                .antMatchers("/**/secured/**").access("hasRole('USER')")
+                //.antMatchers("/**/admin/**").access("hasRole('ADMIN')")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureHandler(getFailureHandler())
+                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-    http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-  }
+        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth
-        .inMemoryAuthentication()
-        .withUser("user").password("password").roles("USER");
-  }
+    @Bean
+    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint() {
+        return new CustomBasicAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler getFailureHandler() {
+        return new CustomAuthFailureHandler();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.debug(true);
+    }
 
 }
