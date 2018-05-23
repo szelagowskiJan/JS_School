f27b5554f10a335d107790626d8f71f60cc502ba
szelagowskiJan <szelagowski.jan1@gmail.com>
2018-05-23 20:58:46 +0200

    work at securityConfig

--- MyProject/src/main/java/pl/coderslab/security/SecurityConfig.java
+++ MyProject/src/main/java/pl/coderslab/security/SecurityConfig.java
@@ -3,12 +3,15 @@ package pl.coderslab.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -19,29 +22,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static String REALM = "MY_TEST_REALM";
 
+
+
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-     //  .antMatchers("/**").permitAll()
-       // .antMatchers("/**/js/**").permitAll()
-                .antMatchers("/**/w3images/**").permitAll()
-               // .antMatchers("/**/secured/**").access("hasRole('ADMIN')")
-                .antMatchers("/**/payment/**").access("hasRole('ADMIN')")
-                //.antMatchers("/**/login/**").access("hasRole('ADMIN')")
+        http.csrf().disable()
+        .authorizeRequests()
+                 .antMatchers("/").permitAll()
+                // .antMatchers("/**").permitAll()
+                 .antMatchers("/**/js/**").permitAll()
+                 .antMatchers("/**/w3images/**").permitAll()
+                 .antMatchers("/**/secured/**").access("hasRole('ADMIN')")
+                .antMatchers("/**/admin/**").access("hasRole('ADMIN')")
+                // .antMatchers("/**/login/**").access("hasRole('ADMIN')")
+                /*.anyRequest().authenticated()*/
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                    .loginPage("/login")
+
                 .failureHandler(getFailureHandler())
                 .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
     }
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint() {
         return new CustomBasicAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
     }
 
     @Bean
@@ -49,10 +68,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new CustomAuthFailureHandler();
     }
 
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return new CustomAccessDeniedHandler();
-    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -66,5 +81,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.debug(true);
     }
-
+/*    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }*/
 }
+
+// .antMatchers("/**/secured/**").access("hasRole('ADMIN')")
+  //       .antMatchers("/**/admin/**").access("hasRole('ADMIN')")
